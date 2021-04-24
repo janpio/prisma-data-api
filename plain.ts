b8712553ca1a149340b2f49fe6d9000cf1a74755
Jan Piotrowski <piotrowski+git@gmail.com>
2021-04-24 14:37:40 +0200

    tiny app using Prisma

---
+++ plain.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+
+  const allUsers = await prisma.user.findMany()
+  console.log({ allUsers })
+
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
