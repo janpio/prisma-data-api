cf528c7332d1babeacaab75fca597ef5574bc4fd
Jan Piotrowski <piotrowski+git@gmail.com>
2021-04-25 01:15:02 +0200

    server: use $executeRaw instead of accidentally present query method

--- server.ts
+++ server.ts
@@ -7,8 +7,9 @@ const app = express()
 
 app.use(express.json())
 
+// Prisma Middleware that takes parameters hidden in where.email and replaces parameter object with them to execute the real query 
 prisma.$use(async (params, next) => {
-  const real_params = params.args.where.email
+  const real_params = JSON.parse(params.args.query)
   console.log(`Query: ${real_params.model}.${real_params.action}(${ JSON.stringify(real_params.args) })`)
 
   // console.log({ params })
@@ -24,7 +25,17 @@ app.post(`/api`, async (req, res) => {
   const body = req.body
   console.log('Request:', JSON.stringify(body))
 
-  const data = await prisma.user.findMany({ where: { email: body }}) // this just uses one random known method, but internally this is rewritten to execute the submitted query
+  // TODO somehow check if schema is up to date (this one vs. one in requesting app)
+
+  // this just uses one random known query method, but is rewritten to execute the real query via the Middleware
+  // the query (received via POS) is submitted via where.email 
+  //const data = await prisma.user.findMany({ where: { email: body }}) 
+  let data
+  try {
+    data = await prisma.$executeRaw(JSON.stringify(body))
+  } catch (error) {
+    data = error
+  }
 
   res.json(data)
 })
@@ -32,6 +43,8 @@ app.post(`/api`, async (req, res) => {
 
 const server = app.listen(3000, () =>
   console.log(`
-🚀 Server ready at: http://localhost:3000`,
+🚀 Server ready at: http://localhost:3000
+Waiting for requests:
+`,
   ),
 )
